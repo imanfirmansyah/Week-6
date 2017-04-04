0124fb1eaf632c1d2b2ef95580086c87055bcc84
Iman firmansyah <bwekz.pirmansyah@gmail.com>
2017-04-04 19:36:56 +0700

    Add files via upload

---
+++ Overriding/MainAyam.java
@@ -0,0 +1,23 @@
+class Hewan {
+
+    public void makan() {
+        System.out.println("Hewan bisa makan");
+    }
+}
+
+class Ayam extends Hewan {
+
+    @Override
+    public void makan() {
+        System.out.println("Ayam bisa makan");
+        super.makan();
+    }
+}
+public class MainAyam {
+    public static void main(String args[]) {
+        Hewan ayam = new Hewan();
+        Ayam ayamKate = new Ayam();
+        ayam.makan();
+        ayamKate.makan();
+    }
+}
